5fafb1fd8fcaae6448e3c90fd75c904cde7bad3d
daVinciBot1495 <cdegranville@rnsoftwareok.com>
2018-12-23 13:40:01 -0800

    Fixed run_game script.

--- run_game.sh
+++ run_game.sh
@@ -2,5 +2,5 @@
 
 set -e
 
-kotlinc MyBot.kt hlt/*.kt
-./halite --replay-directory replays/ -vvv --width 32 --height 32 "java MyBot" "java MyBot"
+kotlinc MyBot.kt hlt/*.kt -include-runtime -d MyBot.jar
+./halite --replay-directory replays/ -vvv --width 32 --height 32 "kotlin -classpath MyBot.jar MyBot" "kotlin -classpath MyBot.jar MyBot"
